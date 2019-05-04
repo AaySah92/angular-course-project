ef0155cf2fd20937be9172188497eba81e6f52ef
Aayush Sahay <aayush.sahay1992@gmail.com>
2019-05-04 17:29:31 +0530

    Added code for navigation based on events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project';
+  currentPage: string;
+  selectPage(selectedPage) {
+    this.currentPage = selectedPage;
+  }
 }
